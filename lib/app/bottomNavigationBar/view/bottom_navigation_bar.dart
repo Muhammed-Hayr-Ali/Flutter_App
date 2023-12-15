05bb4afd8d4d1c8ec58c60ce4463fd557eb351a3
Muhammed-Hayr-Ali <m.thelord963@gmail.com>
2023-12-15 22:55:56 +0300

    first commit

--- lib/app/bottomNavigationBar/view/bottom_navigation_bar.dart
+++ lib/app/bottomNavigationBar/view/bottom_navigation_bar.dart
@@ -1,3 +1,4 @@
+import 'package:application/required_files.dart';
 import 'package:flashy_tab_bar2/flashy_tab_bar2.dart';
 import 'package:application/packages.dart';
 
@@ -21,44 +22,70 @@ class _NavBarState extends State<NavBar> {
     ProfileScreen(),
   ];
 
-  int currentScreen = 0;
+  int currentIndex = 0;
 
-  onItemTapped(int index) {
+  List<BottomNavigationBarItem> items = [
+    BottomNavigationBarItem(
+      icon: SvgPicture.asset(AppAssets.home),
+      activeIcon: SvgPicture.asset(AppAssets.homeActive),
+      label: 'Home',
+    ),
+    BottomNavigationBarItem(
+        icon: SvgPicture.asset(AppAssets.shop),
+        activeIcon: SvgPicture.asset(AppAssets.shopActive),
+        label: 'Brands'),
+    BottomNavigationBarItem(
+        icon: SvgPicture.asset(AppAssets.cart),
+        activeIcon: SvgPicture.asset(AppAssets.cartActive),
+        label: 'Cart'),
+    BottomNavigationBarItem(
+        icon: SvgPicture.asset(AppAssets.userCircle),    activeIcon: SvgPicture.asset(AppAssets.userCircleActive), label: 'Profile'),
+  ];
+
+  void _onTap(int index) {
     setState(() {
-      currentScreen = index;
+      currentIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: view[currentScreen],
-      bottomNavigationBar: FlashyTabBar(
-        backgroundColor: Colors.transparent,
-        animationDuration: const Duration(milliseconds: 100),
-        shadows: const [],
-        selectedIndex: currentScreen,
-        showElevation: true,
-        onItemSelected: (value) => onItemTapped(value),
-        items: [
-          FlashyTabBarItem(
-            icon: const Icon(PhosphorIcons.house),
-            title: Text('Home'.tr),
-          ),
-          FlashyTabBarItem(
-            icon: const Icon(Icons.search),
-            title: const Text('Search'),
-          ),
-          FlashyTabBarItem(
-            icon: const Icon(PhosphorIcons.bell),
-            title: Text('Notifications'.tr),
-          ),
-          FlashyTabBarItem(
-            icon: const Icon(PhosphorIcons.user_circle),
-            title: Text('Profile'.tr),
-          ),
-        ],
+      body: view[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: currentIndex,
+        onTap: (value) => _onTap(value),
+        items: items,
+        showSelectedLabels: false,
+        showUnselectedLabels: false,
+        type: BottomNavigationBarType.fixed,
       ),
     );
   }
 }
+//  FlashyTabBar(
+//         backgroundColor: Colors.transparent,
+//         animationDuration: const Duration(milliseconds: 100),
+//         shadows: const [],
+//         selectedIndex: currentScreen,
+//         showElevation: true,
+//         onItemSelected: (value) => onItemTapped(value),
+//         items: [
+//           FlashyTabBarItem(
+//             icon: const Icon(PhosphorIcons.house),
+//             title: Text('Home'.tr),
+//           ),
+//           FlashyTabBarItem(
+//             icon: const Icon(Icons.search),
+//             title: const Text('Search'),
+//           ),
+//           FlashyTabBarItem(
+//             icon: const Icon(PhosphorIcons.bell),
+//             title: Text('Notifications'.tr),
+//           ),
+//           FlashyTabBarItem(
+//             icon: const Icon(PhosphorIcons.user_circle),
+//             title: Text('Profile'.tr),
+//           ),
+//         ],
+//       ),
--- lib/utils/constants/app_assets.dart
+++ lib/utils/constants/app_assets.dart
@@ -5,9 +5,21 @@ class AppAssets {
   static const String user = 'assets/icons/user.svg';
   static const String camera = 'assets/icons/camera.svg';
   static const String gallery = 'assets/icons/gallery.svg';
-  static const String home = 'assets/icons/home.svg';
   static const String google = 'assets/icons/google.svg';
   static const String keySquare = 'assets/icons/key-square.svg';
   static const String mention = 'assets/icons/mention.svg';
   static const String password = 'assets/icons/password.svg';
+
+  static const String home = 'assets/icons/bottom_navigation_bar/home.svg';
+  static const String homeActive =
+      'assets/icons/bottom_navigation_bar/home_active.svg';
+  static const String cart = 'assets/icons/bottom_navigation_bar/cart.svg';
+  static const String cartActive =
+      'assets/icons/bottom_navigation_bar/cart_active.svg';
+  static const String shop = 'assets/icons/bottom_navigation_bar/shop.svg';
+  static const String shopActive =
+      'assets/icons/bottom_navigation_bar/shop_active.svg';
+  static const String userCircle = 'assets/icons/user-circle/shop.svg';
+  static const String userCircleActive =
+      'assets/icons/bottom_navigation_bar/user-circle_active.svg';
 }
